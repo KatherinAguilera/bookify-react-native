f04c1c66e3bf1d6a9d72d57872adf86c8a9c0f84
Katherine Aguilera <katherine.aguilera@huelladigital.tv>
2021-07-27 17:39:21 -0500

    Use styles

--- src/components/Book/BookListItem.js
+++ src/components/Book/BookListItem.js
@@ -1,11 +1,35 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-ionicons';
 
-export default function BookListItem({book}){
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    paddingVertical: 15,
+    paddingLeft: 15,
+    borderBottomColor: '#E0e0e0',
+    borderBottomWidth: 1,
+  },
+  image:{
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  icon:{
+    color: '#e0ef',
+    marginLeft: 'auto',
+    marginRight: '16',
+  },
+});
+export default function BookListItem({book, onPress}){
   return (
-    <View>
-      <Text>{book.title}</Text>
-    </View>
+    <TouchableOpacity onPress= {onPress}>
+      <View style={styles.container}>
+        <Image source= {{uri: book.cover}} style={styles.image}/>
+        <Text>{book.title}</Text>
+        <Icon name="add-circle" style={styles.icon} />
+      </View>
+    </TouchableOpacity>
   );
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -7,21 +7,28 @@ const BOOK_LIST = [
   {
     id: 1,
     title: 'Hacia rutas salvajes',
+    cover: 'https://picsum.photos/200',
   },
   {
     id: 2,
     title: 'El nombre del viento',
+    cover: 'https://picsum.photos/200',
   },
   {
     id: 3,
     title: 'La aurora',
+    cover: 'https://picsum.photos/200',
   },
   {
     id: 4,
     title: 'Hombres Invisibles',
+    cover: 'https://picsum.photos/200',
   },
 ];
 export default function Home({navigation}){
+  function handleOnPress (){
+    navigation.navigate('Library')
+  }
   return (
     <View>
       {/* <TouchableHighlight onPress={() => navigation.navigate('Library')}>
@@ -29,7 +36,7 @@ export default function Home({navigation}){
       </TouchableHighlight> */}
       <FlatList
         data= {BOOK_LIST}
-        renderItem = {({item}) => <BookListItem book={item} />}
+        renderItem = {({item}) => <BookListItem book={item} onPress= {handleOnPress} />}
         keyExtractor={item=> item.id}
         />
     </View>
